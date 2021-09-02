41dbe591056751aa6e8be380e58ba0c990f5d114
Antique-Alien <82733879+Antique-Alien@users.noreply.github.com>
2021-09-02 15:09:34 -0700

    Add files via upload

---
+++ src/MerkleManager.java
@@ -0,0 +1,12 @@
+public class MerkleManager {
+    public static String enteredWord;
+    public static String expectedWord;
+    public static String rootSetNull;
+    public static int strikes = 0;
+
+    public static void manage(){
+
+
+    }
+
+}
---
+++ src/MerkleManager_Test.java
@@ -0,0 +1,2 @@
+public class MerkleManager_Test {
+}
